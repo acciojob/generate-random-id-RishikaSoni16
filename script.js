0a2fe53092a84775b7936bb7cbd43273ab4acf51
RishikaSoni16 <135811075+RishikaSoni16@users.noreply.github.com>
2024-09-18 17:26:58 +0600

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 function makeid(l) {
   // write your code here
+
+    let result = ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < l; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
 }
+
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
